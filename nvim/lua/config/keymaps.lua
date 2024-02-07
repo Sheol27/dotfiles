426b5728264e7c12db4c0dc343ae0db057cf9e35
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2024-02-07 21:56:13 +0100

    Add files

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Basics
+vim.keymap.set("i", "fj", "<Esc>la")
+vim.keymap.set("i", "fk", "<C-o>A")
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "JJ", "<Esc>")
