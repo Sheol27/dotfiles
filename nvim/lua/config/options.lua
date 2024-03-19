aded19cb47ac3e0f10078105bc910c26fe90c051
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2024-03-19 10:55:48 +0100

    Updates

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,16 @@
 vim.opt.winbar = "%=%m %f"
 vim.opt.mouse = ""
 vim.o.termguicolors = true
+
+function ChangeLspServerPort(lspServerName, newPort)
+  -- Stop the specified LSP server
+  vim.cmd("LspStop " .. lspServerName)
+  -- Give it a moment to stop; adjust as necessary
+  vim.wait(500, function() end)
+  -- Setup the LSP server again with the new port
+  require("lspconfig")[lspServerName].setup({
+    cmd = { "nc", "localhost", tostring(newPort) },
+  })
+  -- Start the LSP server again
+  vim.cmd("LspStart " .. lspServerName)
+end
