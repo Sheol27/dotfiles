db8bb3374d32ad93cfac4638e30592b54a8ed591
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2024-10-04 10:25:28 +0200

    Change nvim colorscheme and upgrade plugins

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -16,19 +16,17 @@ return {
     priority = 1000,
     config = function()
       vim.g.sonokai_enable_italic = true
-      vim.g.sonokai_style = "atlantis"
+      vim.g.sonokai_style = "shusia"
+      vim.g.sonokai_transparent_background = 2
+      vim.g.sonokai_diagnostic_text_highlight = 1
+      vim.g.sonokai_diagnostic_virtual_text = "colored"
+      -- vim.g.sonokai_inlay_hints_background = "dimmed"
     end,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      transparent = true,
-    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-frappe",
+      colorscheme = "sonokai",
     },
   },
 }
