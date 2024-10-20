c683726f9783b1bdf12af97e18341912cbc1ce51
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2024-10-20 23:04:55 +0200

    Update keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,17 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Basics
-vim.keymap.set("i", "fj", "<Esc>la")
-vim.keymap.set("i", "fk", "<C-o>A")
-vim.keymap.set("i", "jj", "<Esc>")
-vim.keymap.set("i", "JJ", "<Esc>")
-vim.keymap.set("t", "JJ", "<C-\\><C-n>", { noremap = true })
-vim.keymap.set("t", "jj", "<C-\\><C-n>", { noremap = true })
-
 vim.keymap.set("n", "<Ctrl-d>", "<Ctrl-d>zz")
 vim.keymap.set("n", "<Ctrl-u>", "<Ctrl-u>zz")
 vim.keymap.set("n", "<leader>fx", ":!chmod +x %<CR>", { desc = "Make file executable" })
+
+local lazyterm = function()
+  LazyVim.terminal(nil, { cwd = LazyVim.root() })
+end
+
+vim.keymap.set("n", "<Esc><cr>", lazyterm, { desc = "Terminal (Root Dir)" })
+vim.keymap.set("t", "<Esc><cr>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 
 -- Lua function to capitalize each word in selection
 function capitalize_words()
