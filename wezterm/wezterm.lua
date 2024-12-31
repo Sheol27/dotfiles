922ed584bfb1305d375563da7d65b603a51f5fa4
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2024-12-31 09:55:30 +0100

    Make wezterm fullscreen by default

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,8 +1,14 @@
 local wezterm = require("wezterm")
-
+local mux = wezterm.mux
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+config.window_close_confirmation = "NeverPrompt"
+config.color_scheme = "Catppuccin Frappe"
 
 config.font = wezterm.font("FiraCode Nerd Font Mono")
 config.font_size = 14
