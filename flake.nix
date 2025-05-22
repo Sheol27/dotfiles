3fea010f5c332e3d16223df56c5b3149e1b49dee
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2025-05-22 23:02:17 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "My development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/72841a4a8761d1aed92ef6169a636872c986c76d";  # pin to exact commit
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          neovim         
+          ripgrep
+          fd
+          fzf
+          lazygit
+          nodejs
+          unzip
+          python3
+        ];
+      };
+    }
+}
+
